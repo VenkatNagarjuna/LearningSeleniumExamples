066997f6a3b133388a1c791568ab0755254f1977
Venkata Nagarjuna <Venkata.Madineni@ATMECS.CORP>
2016-06-25 11:56:21 +0530

    Add atmecs test class

---
+++ src/test/java/com/learnsel/pageFactory/AtmecsTest.java
@@ -0,0 +1,12 @@
+package com.learnsel.pageFactory;
+
+import org.testng.annotations.Test;
+
+public class AtmecsTest {
+	
+	@Test
+	public void atmecs(){
+		System.out.println("Your are working for ATMECS");
+	}
+
+}
